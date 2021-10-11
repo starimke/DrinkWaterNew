999b29808fa858256618a77e558c481e83b2993a
STARIMK <starimk@naver.com>
2021-10-11 23:08:35 +0900

    Initial Commit

---
+++ DrinkWaterNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrinkWaterNew
+//
+//  Created by 심민규 on 2021/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
